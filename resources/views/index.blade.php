7343139fd916e082a97f9e3cc449d0ed94ef0ae9
Nitin Gupta <56685174+guptanitin4647@users.noreply.github.com>
2022-12-30 11:11:47 +0000

    initialize

---
+++ resources/views/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="bg-image" style="background-image: url()">
+    <div style="color: #f8fafc
+    padding-top: 10px
+    justify-content: center">
+        <div style="padding-top: 4rem
+        padding-bottom: 16rem
+        margin: auto
+        width: 80%
+        display: block">
+            <h1 style="font-size: 5rem
+            text-transform: uppercase
+            font-weight: bold
+            text-shadow: 0 1px 3px rgba(0, 0, 0, 0.3)
+            color: #ffffff
+            padding-bottom: 14px
+            ">
+                do you want to become a developer
+            </h1>
+            <a href="/blog"
+            style="text-align: center;
+            background-color: #eaeaea;
+            color: #757575;
+            padding: 2px;
+            padding-left: 4px;
+            font-weight: bold;
+            text-transform: uppercase;
+            font-size: 1.25rem;">
+                Read more
+            </a>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,122 @@
+<link rel="stylesheet" type="text/css" href="{{ asset('resources/css/app.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-white text-muted w-4/5">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block ">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+   
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="col-md-12">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <!-- <i class="fas fa-gem me-3 text-secondary"></i> -->
+            Pages
+          </h6>
+          <p class="col-md-4">
+            
+          <p>
+            <a href="#!" class="text-reset">Home</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Blog</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Login</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Register</a>
+          </p>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Latest posts
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Why we Love Tech</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Why we use larvel</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Why php is best</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Why we love design</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+         
+
+             <!-- Grid column -->
+             <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            info@example.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/"></a>
+  </div>
+  <!-- Copyright -->
+</footer>
